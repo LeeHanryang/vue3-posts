255cfd665cc58cede1a221c3f693b113cce5c6b0
이준혁 <ballack02@naver.com>
2025-05-13 18:09:24 +0900

    소셜 로그인 버튼의 URL에서 슬래시 제거

--- src/components/auth/SocialLoginButtons.vue
+++ src/components/auth/SocialLoginButtons.vue
@@ -4,7 +4,7 @@
 			<span>소셜 계정으로 시작하기</span>
 		</div>
 		<div class="d-grid gap-2">
-			<a :href="`${apiUrl}/oauth2/authorization/google`" class="btn btn-google">
+			<a :href="`${apiUrl}oauth2/authorization/google`" class="btn btn-google">
 				<img
 					src="https://www.google.com/favicon.ico"
 					alt="Google"
@@ -12,7 +12,7 @@
 				/>
 				Google로 시작하기
 			</a>
-			<a :href="`${apiUrl}/oauth2/authorization/naver`" class="btn btn-naver">
+			<a :href="`${apiUrl}oauth2/authorization/naver`" class="btn btn-naver">
 				<img
 					src="https://www.naver.com/favicon.ico"
 					alt="Naver"
@@ -20,7 +20,7 @@
 				/>
 				Naver로 시작하기
 			</a>
-			<a :href="`${apiUrl}/oauth2/authorization/kakao`" class="btn btn-kakao">
+			<a :href="`${apiUrl}oauth2/authorization/kakao`" class="btn btn-kakao">
 				<img
 					src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
 					alt="Kakao"
